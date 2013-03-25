0e7b167fc9e71bf6ca4aa5a49bd4ba7134e98162
John Lagos <john_lagos@hotmail.com>
2013-03-24 20:38:12 -0400

    through 10

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,26 @@
+class MicropostsController < ApplicationController
+  before_filter :signed_in_user, only: [:create, :destory]
+  before_filter :correct_user, only: :destroy
+
+  def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      redirect_to root_path, :flash => { :success => "Micropost created!" }
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @micropost.destroy
+    redirect_to root_path, :flash => { :success => "Micropost deleted!" }
+  end
+
+  private
+
+    def correct_user
+      @micropost = current_user.microposts.find_by_id(params[:id])
+      redirect_to root_url if @micropost.nil?
+    end
+end
